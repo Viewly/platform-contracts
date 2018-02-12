1d7bbb967a532ef3ee539c9df58baf3e02fd9fd3
furion <_@furion.me>
2018-02-12 16:38:36 +0100

    Add networks to Truffle config
    * Ganache, Mainnet and Kovan networks added (parity based)
    * Remove unecessary truffle-config.js file

--- truffle.js
+++ truffle.js
@@ -1,4 +1,21 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+    networks: {
+        ganache: {
+            host: "127.0.0.1",
+            port: 7545,
+            network_id: "*" // Match any network id
+        },
+        mainnet: {
+            network_id: 1,
+            host: "127.0.0.1",
+            port: 8545,
+        },
+        kovan: {
+            network_id: 42,
+            host: "127.0.0.1",
+            port: 8545,
+        }
+    }
 };

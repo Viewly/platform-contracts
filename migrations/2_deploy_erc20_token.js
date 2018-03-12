a672049a0fca2e908b993cec8e37da6a594e779a
furion <_@furion.me>
2018-03-12 23:59:34 +0100

    Modify migrations to use View Token on mainnet

--- migrations/2_deploy_erc20_token.js
+++ migrations/2_deploy_erc20_token.js
@@ -1,7 +1,9 @@
 var DSToken = artifacts.require("./dappsys/DSToken.sol");
 var DSGuard = artifacts.require("./dappsys/DSGuard.sol");
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network) {
+    if (network == "mainnet")
+        return
     return deployer.deploy(DSGuard)
         .then(() => deployer.deploy(DSToken, 'VIEW'))
         .then(() => DSToken.deployed())
--- migrations/3_deploy_video_publisher.js
+++ migrations/3_deploy_video_publisher.js
@@ -1,6 +1,9 @@
 var DSToken = artifacts.require("./dappsys/DSToken.sol");
 var VideoPublisher = artifacts.require("./VideoPublisher.sol");
 
-module.exports = function(deployer) {
-    deployer.deploy(VideoPublisher, DSToken.address, web3.toWei('10', 'ether'))
+module.exports = function(deployer, network) {
+    const viewTokenAddr = (network == "mainnet") ?
+        "0xf03f8d65bafa598611c3495124093c56e8f638f0" : DSToken.address;
+    console.log(viewTokenAddr)
+    deployer.deploy(VideoPublisher, viewTokenAddr, web3.toWei('10', 'ether'))
 };

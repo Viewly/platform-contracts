bc26b7619ba6a432f7720aa5f91944d35c12639c
furion <_@furion.me>
2018-06-24 22:01:05 +0200

    Add Ethereum support to VideoPublisher

--- contracts/VideoPublisher.sol
+++ contracts/VideoPublisher.sol
@@ -4,38 +4,55 @@ import "./dappsys/math.sol";
 import "./dappsys/token.sol";
 import "./dappsys/auth.sol";
 
-// This contract allows people to use VIEW tokens to publish videos on Viewly.
+// Pay to publish videos on Viewly.
 contract VideoPublisher is DSAuth, DSMath {
 
     DSToken public viewToken;
-    uint public price;
+    uint public priceView;
+    uint public priceEth;
     mapping (bytes12 => bool) public videos;
     event Published(
         bytes12 videoID,
-        uint price
+        address publisher
     );
 
-    function VideoPublisher(DSToken viewToken_, uint price_) public {
+    function VideoPublisher(
+        DSToken viewToken_,
+        uint priceView_,
+        uint priceEth_) public {
         viewToken = viewToken_;
-        price = price_;
+        priceView = priceView_;
+        priceEth = priceEth_;
     }
 
     function publish(bytes12 videoID) public {
         require(!videos[videoID]);
-        require(viewToken.transferFrom(msg.sender, this, price));
+        if (msg.value == 0) {
+            require(viewToken.transferFrom(msg.sender, this, priceView));
+        } else {
+            require(msg.value >= priceEth);
+        }
         videos[videoID] = true;
-        Published(videoID, price);
+        Published(videoID, msg.sender);
     }
 
-    function setPrice(uint newPrice) public auth {
-        price = newPrice;
+    function setPrices(uint priceView_, uint priceEth_) public auth {
+        priceView = priceView_;
+        priceEth = priceEth_;
     }
 
     function withdraw(address addr) public auth {
-        viewToken.transfer(addr, viewToken.balanceOf(this));
+        uint tokenBalance = viewToken.balanceOf(this);
+        if (tokenBalance > 0) {
+            viewToken.transfer(addr, tokenBalance);
+        }
+        if (this.balance > 0) {
+            addr.transfer(this.balance);
+        }
     }
 
     function destruct(address addr) public auth {
+        require(this.balance == 0);
         require(viewToken.balanceOf(this) == 0);
         selfdestruct(addr);
     }
